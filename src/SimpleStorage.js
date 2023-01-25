567b08ffd884b3170875d54e2e3f7d88d193dc4e
Nawar Hisso <nawar@polytrade.finance>
2023-01-25 21:24:48 +0400

    :memo: Comment The Code

--- src/SimpleStorage.js
+++ src/SimpleStorage.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+import {
+  Container,
+  TextField,
+  Button,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
 import Token_ABI from "./configs/ERC20_ABI.json";
 import Storage_ABI from "./configs/Storage_ABI.json";
 
@@ -36,45 +38,62 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const SimpleStorage = (props) => {
+const SimpleStorage = () => {
+  // State to store the user's address
   const [address, setAddress] = useState("");
+  // State to store the data stored on the contract
   const [data, setData] = useState("");
+  // State to store the balance of the user's address
   const [balance, setBalance] = useState(0);
   const classes = useStyles();
 
+  /**
+   * @async
+   * Function to get the user's address from ethereum provider
+   */
+  const getAddress = async () => {
+    // Request the user wallet's address
+    const addresses = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+
+    // Set the user's address in the component's state
+    setAddress(addresses[0]);
+  };
+
+  /**
+   * @async
+   * Function to get the balance of the user's address
+   */
   const getBalance = async () => {
-    console.log("balance");
+    // Connect to the network by creating an instance of a new provider
     const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    // Create an instance of the ERC-20 token contract
     const contract = new ethers.Contract(
       "0x0000000000000000000000000000000000001010",
       Token_ABI,
       provider
     );
 
+    // Call the balanceOf() function of the contract to get the user's balance
     let myBalance = await contract.balanceOf(address);
     myBalance = myBalance.toString();
     console.log(myBalance);
+    // Format the balance to a number
     myBalance = Number(ethers.utils.formatUnits(myBalance, 18));
+    // Update the balance state
     setBalance(myBalance);
   };
 
-  useEffect(() => {
-    const getAddress = async () => {
-      const addresses = await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
-
-      setAddress(addresses[0]);
-    };
-
-    getAddress();
-  }, []);
-
-  useEffect(() => {
-    // Connect to the Ethereum network
+  /**
+   * @async
+   * A function to read data from the SimpleStorage contract
+   */
+  const readData = async () => {
+    // Connect to the network by creating an instance of a new provider
     const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-    // Create an instance of the contract
+    // Create an instance of the SimpleStorage contract
     const contract = new ethers.Contract(
       "0x5366B849A4b702F82E4746dBc8A32b4E674Fbd9A",
       Storage_ABI,
@@ -83,29 +102,42 @@ const SimpleStorage = (props) => {
 
     // Call the read function to get the current value of data
     contract.read().then((result) => {
+      // Update the data state
       setData(result);
     });
-  }, []);
+  };
 
+  /**
+   * @async
+   * Handle the form submit event and write data to the contract
+   * @param {Event} event - The form submit event
+   */
   const handleWrite = async (event) => {
     event.preventDefault();
-    // Connect to the Ethereum network
+    // Connect to the network by creating an instance of a new provider
     const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // Create an instance to the signer because the write function cannot be done by only the provider
     const signer = provider.getSigner();
 
-    // Create an instance of the contract
+    // Create an instance of the SimpleStorage contract using the signer
     const contract = new ethers.Contract(
       "0x5366B849A4b702F82E4746dBc8A32b4E674Fbd9A",
       Storage_ABI,
       signer
     );
+    // Get the new data from the form
     const newData = event.target.data.value;
 
-    // Call the write function to update the value of data
+    // Call the write function to update the value of data on the contract
     // await contract.write(newData);
-
     contract.write(newData).then((txn) => console.log(txn.hash));
   };
+
+  // Use effect hook to fetch the user's address and read data stored on the contract
+  useEffect(() => {
+    getAddress();
+    readData();
+  }, []);
 
   return (
     <Container maxWidth="sm" className={classes.container}>
